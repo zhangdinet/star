bb9b9ccbebeb3cfe733afe116a520d35d54784bd
zhangdi <zhangdi@ifpnq.com>
2015-08-08 16:28:03 +0800

    add logback

--- src/main/java/com/star/HomeController.java
+++ src/main/java/com/star/HomeController.java
@@ -1,5 +1,8 @@
 package com.star;
 
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +11,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    private Logger logger = LoggerFactory.getLogger(HomeController.class);
+
 	@RequestMapping(method = RequestMethod.GET)
-	public String printWelcome(ModelMap model) {
+	public String index(ModelMap model) {
+        logger.debug("call index");
 		model.addAttribute("message", "Hello Wrold, Success!");
+        logger.info("index finished");
 		return "home";
 	}
+
 }
